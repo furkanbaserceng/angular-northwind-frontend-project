ab5632001524d14da368f90d086d9f0792063a30
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-28 19:02:15 +0300

    Added fake categories data and viewed.

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
+
+  category:any={categoryId:1,categoryName:"Bilgisayar"};
+  category2:any={categoryId:2,categoryName:"Cep Telefonu"};
+  category3:any={categoryId:3,categoryName:"Ev Ürünleri"};
+  category4:any={categoryId:4,categoryName:"Yiyecek"};
+  category5:any={categoryId:5,categoryName:"İçecek"};
+
+  categories=[this.category,this.category2,this.category3,this.category4,this.category5];
+
+
   constructor() { }
 
   ngOnInit(): void {
